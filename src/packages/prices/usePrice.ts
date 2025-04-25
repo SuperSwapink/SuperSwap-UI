6f5607508684ea10ad0982d5bb227e59f36cb87e
SuperSwapink <team@superswap.ink>
2025-04-25 11:56:26 -0400

    update

--- src/packages/prices/usePrice.ts
+++ src/packages/prices/usePrice.ts
@@ -1,32 +1,38 @@
-import { useQuery } from "@tanstack/react-query"
-import { ChainId } from "../chain"
-import axios from "axios"
-import useTokenPrices from "@/hooks/useTokenPrices"
+import { useQuery } from "@tanstack/react-query";
+import { ChainId } from "../chain";
+import axios from "axios";
+import useTokenPrices from "@/hooks/useTokenPrices";
+import { USDC } from "../currency";
 
 interface UsePrice {
-  chainId: ChainId | undefined
-  address: string | undefined
-  enabled?: boolean
+  chainId: ChainId | undefined;
+  address: string | undefined;
+  enabled?: boolean;
 }
 
 export const usePrice = ({ chainId, address, enabled }: UsePrice) => {
-  const { setPrices } = useTokenPrices()
+  const { setPrices } = useTokenPrices();
   return useQuery({
     queryKey: [chainId, address],
     queryFn: async () => {
-      if (!chainId) return 0
+      if (!chainId) return 0;
+      if (
+        chainId === 1 &&
+        address?.toLowerCase() === USDC[ChainId.ETHEREUM].address.toLowerCase()
+      )
+        return 1;
       const { data } = await axios.get(
         `https://api.dexscreener.com/latest/dex/tokens/${address}`
-      )
-      const price = Number(data?.pairs?.[0]?.priceUsd ?? "0")
+      );
+      const price = Number(data?.pairs?.[0]?.priceUsd ?? "0");
       setPrices((prices: any) => ({
         ...prices,
         [`${chainId}:${address?.toLowerCase()}`]: price,
-      }))
-      return price
+      }));
+      return price;
     },
     enabled: Boolean(chainId && address) && Boolean(enabled),
     staleTime: 900000, // 15 mins
     refetchOnWindowFocus: false,
-  })
-}
+  });
+};
