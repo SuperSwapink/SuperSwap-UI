3e7aad0d8c65cc1cbdc25daa999d8e1efeb8818d
SuperSwapink <team@superswap.ink>
2025-05-25 18:22:38 -0400

    update

--- src/packages/router/liquidity-providers/UniswapV3.ts
+++ src/packages/router/liquidity-providers/UniswapV3.ts
@@ -15,6 +15,7 @@ export class UniswapV3Provider extends UniswapV3BaseProvider {
       [ChainId.WORLDCHAIN]: "0x7a5028BDa40e7B173C278C5342087826455ea25a",
       [ChainId.BASE]: "0x33128a8fC17869897dcE68Ed026d694621f6FDfD",
       [ChainId.ARBITRUM]: "0x1F98431c8aD98523631AE4a59f267346ea31F984",
+      [ChainId.BLAST]: "0x792edAdE80af5fC680d96a2eD80A44247D2Cf6Fd",
       [ChainId.ZORA]: "0x7145F8aeef1f6510E92164038E1B6F8cB2c42Cbb",
     } as const;
 
@@ -35,6 +36,8 @@ export class UniswapV3Provider extends UniswapV3BaseProvider {
         "0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54",
       [ChainId.ARBITRUM]:
         "0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54",
+      [ChainId.BLAST]:
+        "0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54",
       [ChainId.ZORA]:
         "0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54",
     } as const;
@@ -48,6 +51,7 @@ export class UniswapV3Provider extends UniswapV3BaseProvider {
       [ChainId.WORLDCHAIN]: "0xE61df0CaC9d85876aCE5E3037005D80943570623",
       [ChainId.BASE]: "0x0CdeE061c75D43c82520eD998C23ac2991c9ac6d",
       [ChainId.ARBITRUM]: "0xbfd8137f7d1516D3ea5cA83523914859ec47F573",
+      [ChainId.BLAST]: "0x2E95185bCdD928a3e984B7e2D6560Ab1b17d7274",
       [ChainId.ZORA]: "0x209AAda09D74Ad3B8D0E92910Eaf85D2357e3044",
     } as const;
 
